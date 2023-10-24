6538a4561347c99636ba46340fcf887f02e9e6ee
Kali <kalinkanery@gmail.com>
2023-10-24 20:56:44 -0300

    tratar erro

--- modulo-1/projeto-individual/projeto-individual.js
+++ modulo-1/projeto-individual/projeto-individual.js
@@ -4,14 +4,14 @@
 Rede de lojas de automóveis (CarStore)
 
 Enviar um e-mail, todas as segundas-feiras, para os clientes que visitaram as lojas no último mês, 
-informando-os sobre os novos veículos e os mais vendidos, bem como as condições para aquisição (sejam criativos).
+ok informando-os sobre os novos veículos e os mais vendidos, bem como as condições para aquisição (sejam criativos).
 
-- Requerir a função no arquivo "envia-email.js", que fará o envio "fake".
-- Criar uma lista (array) contendo emails dos clientes e uma flag com a decisão sobre receber ou não comunicações.
-1. Criar uma função para verificar o dia da semana atual, que será levado em conta para o disparo dos emails.
-2. Criar uma função para montar o corpo do e-mail a ser enviado.
-3. Criar uma função para enviar o e-mail para cada um dos clientes da lista, levando em conta a sua decisão sobre receber comunicações de marketing.
-4. Tratar o retorno de erro ou sucesso da função "enviarEmail", de maneira a exibir uma mensagem amigável ao usuário no console.
+-ok Requerir a função no arquivo "envia-email.js", que fará o envio "fake".
+-ok Criar uma lista (array) contendo emails dos clientes e uma flag com a decisão sobre receber ou não comunicações.
+1.ok Criar uma função para verificar o dia da semana atual, que será levado em conta para o disparo dos emails.
+2.ok Criar uma função para montar o corpo do e-mail a ser enviado.
+3.ok Criar uma função para enviar o e-mail para cada um dos clientes da lista, levando em conta a sua decisão sobre receber comunicações de marketing.
+4.ok Tratar o retorno de erro ou sucesso da função "enviarEmail", de maneira a exibir uma mensagem amigável ao usuário no console.
 */
 //
 
@@ -21,16 +21,25 @@ const listaClientes = require("./clientes");
 
 
 function ehSegundaFeira() {
-    return new Date().getDay() === 1
+    return true
+    return new Date().getDay() === 1;
 }
-ehSegundaFeira();
 
+function tratarRetorno (resposta, nome) {
+    if (resposta.status === "Error") {
+        console.error(`Falha no envio do e-mail para ${nome}: ${resposta.message}`)
+    }
+resposta.message
 
-function enviaNewsletter (clientes) {
-    for (const cliente of clientes) {
-        if (cliente.flag === "s") {
-            const resposta = enviarEmail (cliente.email, "Newsletter CarStore", montarCorpoEmail(cliente.nome));
-            console.log(resposta.status, resposta.message);
+}
+
+function enviaNewsletter(clientes) {
+    if (ehSegundaFeira()) {
+        for (const cliente of clientes) {
+            if (cliente.flag === "s") {
+                const resposta = enviarEmail(cliente.email, "Newsletter CarStore", montarCorpoEmail(cliente.nome));
+                tratarRetorno(resposta, cliente.nome);
+            }
         }
     }
 }
