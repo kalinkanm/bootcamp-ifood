ed57c9382e586c0b727cfd87121a969c9b8cc4ed
Kali <kalinkanery@gmail.com>
2023-10-24 16:36:53 -0300

    grupo 2

--- modulo-1/projeto-grupo/projeto-grupo.js
+++ modulo-1/projeto-grupo/projeto-grupo.js
@@ -1,12 +1,4 @@
-/*
-TODO LIST
-Funcionalidades:
-- Adicionar uma tarefa
-- Editar uma tarefa salva
-- Remover uma tarefa salva
-- Listar todas as tarefas salvas
-- Obter uma tarefa, através de um parâmetro (id)
-*/
+// GRUPO 2: Alvaro, Everton, Gabriel Hein, Kalinka
 
 const opcoes = `Opções: 
 1 Adicionar uma tarefa
